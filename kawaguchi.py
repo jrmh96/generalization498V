42f384ea8b2359574e57992551e6df5013968fc0
Jeremy Hu <jrmh96@gmail.com>
2018-12-11 14:10:50 -0500

    calculate G over test set

--- kawaguchi.py
+++ kawaguchi.py
@@ -26,12 +26,15 @@ class MLP(nn.Module):
         # x = self.fc3(x)
         return self.fc3(x) # last layer activation function is identity y=x
 
-    def evaluateZ(self, x): # right now, x is the activation on the last layer
+    def evaluateZ(self, x):
         # print(x.shape)
         x = self.forward(x)
         z = x.data.numpy()
-        # z = x.detach().numpy()
-        G = np.dot(z.T,z)
+        # print(np.shape(z))
+        # G = 1.0/len(mnist_val) * np.dot(z, z.T)
+        G = np.dot(z.T, z)  # z is 1xn vector
+        # G = np.multiply(z, z.T)
+        # G not normalized here ....
         values, vectors = LA.eig(G)
         return (np.sum(values,axis=0))
 
@@ -68,11 +71,13 @@ if __name__ == '__main__':
         ])),
         batch_size=batch_size, shuffle=True)
 
-    validation_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('../data', train=False, transform=transforms.Compose([
+    mnist_val = datasets.MNIST('../data', train=False, transform=transforms.Compose([
                            transforms.ToTensor(),
                           # transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
+                       ]))
+
+    validation_loader = torch.utils.data.DataLoader(
+        mnist_val,
         batch_size=1, shuffle=False)
 
     interval = 500
@@ -81,17 +86,21 @@ if __name__ == '__main__':
     loss_fn = torch.nn.MSELoss(reduction='sum')
     p = 0
 
+    testLoss = []  # Training eigen values vs. loss
+    testEigenVals = []
+
     for epoch in range(0,3):
         model.train()
         for batch_idx, (data, target) in enumerate(train_loader):
-            # if batch_idx > 5:
-              #  break
+            # if batch_idx > 2:
+            #  break
+
             model.train()
             target = encode(target)
 
             optimizer = optim.Adam(model.parameters(), lr=0.0005)
 
-            # use this one to get bad test errors:
+            # use this one to get bad accuracy:
             # optimizer = optim.Adam(model.parameters(), lr = 0.1)
 
             output = model(data)
@@ -109,53 +118,78 @@ if __name__ == '__main__':
 
         val_loss, correct = 0, 0
 
-        testLoss = []  # Training eigen values vs. loss
-        testEigenVals = []
+        correctLoss = []
+        correctEig = []
+        incorrectLoss=[]
+        incorrectEig = []
 
+        zSum = None
+        e = 0
         for data, target in validation_loader: # Validation error
 
             output = model(data)
-
             pred = output.data.max(1)[1]  # get the index of the max probability
 
-            eigen = model.evaluateZ(data) # sum of eigen values of the output vector zT x z
+            #if e > 2:
+            #    break
+            #e+=1
+
+            z = output.data.numpy()
+            zzT = np.dot(z.T, z)
+
+            if zSum is None:
+                zSum = zzT
+            else:
+                zSum += zzT
 
             # print("Eigen: %f" % eigen)
             # print("Loss: %f" % float(loss.item()))
 
-            if pred == target.item():
-                correct += pred.eq(target.data).data.sum().item()
             # val_loss = loss_fn(output,target)
             encodedTarget = encode(target)
             val_loss = loss_fn(output,encodedTarget)
 
-            testLoss.append(val_loss.item())
-            testEigenVals.append(eigen)
+            if pred == target.item():
+                correct += pred.eq(target.data).data.sum().item()
+                # correctEig.append(eigen)
+                # correctLoss.append(val_loss.item())
 
-        # val_loss /= len(validation_loader)
+            # testLoss.append(val_loss.item())
+            # testEigenVals.append(eigen)
 
         accuracy = 100. * correct / len(validation_loader.dataset)
 
-        # plot loss (x) vs. G eigenvalue sums (y):
-        print(max(testLoss))
-        print(max(testEigenVals)) # why are there so many fewer when high loss?
+        values, vectors = LA.eig(1.0 / len(mnist_val) * zSum)
+
+        eigen = np.sum(values, axis=0)
+        testLoss.append(val_loss.item())
+        testEigenVals.append(eigen)
+        print(testLoss)
+        print(testEigenVals)
         plt.scatter(testLoss, testEigenVals)
-        plt.title('Eigenvalues vs. Loss on test set at end of epoch {}'.format(epoch))
+        plt.title('Eigenvalues vs. Loss on test set at end of epoch {}, accuracy: {}'.format(epoch, accuracy))
         plt.xlabel('Test Loss')
         plt.ylabel('Eigenvalues of h.T*h') # h = activation vector of last layer
         plt.show()
+
+        '''
+        plt.scatter(correctLoss, correctEig)
+        plt.title('Correctly classified eigenvalues vs. Loss \n on test set at end of epoch {}, accuracy: {}'.format(epoch, accuracy))
+        plt.xlabel('Test Loss')
+        plt.ylabel('Eigenvalues of h.T*h')  # h = activation vector of last layer
+        plt.show()
         plt.gcf().clear()
+
+        plt.scatter(incorrectLoss, incorrectEig)
+        plt.title('Incorrect classified eigenvalues vs. Loss \n on test set at end of epoch {}, accuracy: {}'.format(epoch, accuracy))
+        plt.xlabel('Test Loss')
+        plt.ylabel('Eigenvalues of h.T*h')  # h = activation vector of last layer
+        plt.show()
+        plt.gcf().clear()
+        '''
+
         print("\n\n********EPOCH********* {} done".format(epoch))
         print("Accuracy: %f %%\n\n" % accuracy)
-
-    parameters = []
-
-    '''
-    for param in model.parameters():
-        if(len(param.size()) == 2):
-            print(type(param.data), param.size())
-            parameters.append(param.data)
-    '''
 
 plt.close('all')
 
